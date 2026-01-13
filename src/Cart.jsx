546a2b25952a9b25a76a73493b876f9daf318a82
obrona <tlim8772@gmail.com>
2026-01-13 10:50:41 +0800

    add cart

---
+++ src/Cart.jsx
@@ -0,0 +1,69 @@
+import { useOutletContext } from "react-router";
+import styles from './Cart.module.css';
+
+export function Cart() {
+  const props = useOutletContext();
+  const {
+    products,
+    cart,
+    updateItemCnt,
+    decrementItemCnt,
+    incrementItemCnt,
+  } = props;
+
+  return (
+    <div className={styles.cart}>
+      <table className={styles.cartTable}>
+        <caption>Your Cart</caption>
+        <colgroup>
+          <col />
+          <col />
+          <col />
+        </colgroup>
+        <tbody>
+          {
+            Array.from(cart.entries()).map(([itemId, cnt]) => {
+              const product = products.find(p => p.id === itemId);
+              return (
+                <tr key={itemId}>
+                  <td>
+                    {product.title}
+                  </td>
+                  <td className={styles.tdCenter}>
+                    <div className={styles.gutter}>
+                      <button onClick={decrementItemCnt(itemId)}>-</button>
+                      {cnt}
+                      <button onClick={incrementItemCnt(itemId)}>+</button>
+                    </div>
+                  </td>
+                  <td className={styles.tdCenter}>
+                    {(cnt * product.price).toFixed(2)}
+                  </td>
+                </tr>
+              )
+            })
+          }
+          <tr>
+            <td>Total</td>
+            <td>&nbsp;</td>
+            <td className={styles.tdCenter}>
+              {
+                Array.from(cart.entries()).reduce((acc, [itemId, cnt]) => {
+                  const product = products.find(p => p.id === itemId);
+                  return acc + cnt * product.price;
+                }, 0).toFixed(2)
+              }
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td className={styles.tdCenter}>
+              <button>Checkout</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import { Shop } from './Shop.jsx';
+import { Cart } from './Cart.jsx';
 
 const router = createBrowserRouter([
   {
@@ -21,7 +22,7 @@ const router = createBrowserRouter([
       },
       {
         path: 'cart',
-        element: <div>Cart</div>
+        element: <Cart />
       }
     ]
   }
